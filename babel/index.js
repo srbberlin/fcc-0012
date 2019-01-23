cbc953eb489ad59cd217206e855ee53ddc267a2c
Hans Schrieber <info@hans-schrieber.de>
2019-01-23 15:03:03 +0100

    Update index.js

--- babel/index.js
+++ babel/index.js
@@ -113,6 +113,8 @@ class Root extends React.Component {
     this.over = this.over.bind(this);
     this.out = this.out.bind(this);
     this.state = {
+      apiRecent: 'https://api.npoint.io/df4d86c7cd7b69ad3603',
+      apiAlltime: 'https://api.npoint.io/b56606bb3536eb445851',
       api: 'https://fcctop100.herokuapp.com/api/fccusers/top/',
       which: 'recent',
       down: true,
@@ -124,7 +126,7 @@ class Root extends React.Component {
 
   getData (which) {
     $.ajax({
-      url: this.state.api + which,
+      url: which ==='recent' ? this.state.apiRecent : this.state.apiAlltime,
       dataType: 'json', 
       cache: false,
       success: function(data) {
